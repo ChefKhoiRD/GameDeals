deee463d0b951ae727d3bd1c91300dc44421bbe2
Gromit <eversoryan@gmail.com>
2022-08-03 19:00:22 -0600

    Added helper require in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers')
+const helpers = require('./')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Using express and giving a port
